40ac0404503174dc41f04efdab6a379054b902f2
JayminPatel18 <pateljaymin551@gmail.com>
2023-01-31 13:46:31 +0530

    change

--- bin/main.dart
+++ bin/main.dart
@@ -18,8 +18,36 @@ void main() {
   // print(s1 + ' ' + s2);
 
   // 3 : Row String
-  var s3 = 'In a raw string, not even \n gets special treatment';
-  var s4 = r'In a raw string, not even \n gets special treatment';
-  print(s3);
-  print(s4);
+  // var s3 = 'In a raw string, not even \n gets special treatment';
+  // var s4 = r'In a raw string, not even \n gets special treatment';
+  // print(s3);
+  // print(s4);
+
+  // 4 String Properties and Methods
+  // var abc = 'Hello';
+  // print(abc.length);
+  // print(abc.isEmpty);
+  // print(abc.isNotEmpty);
+  // print(abc.toLowerCase());
+  // print(abc.toUpperCase());
+  // print(abc.contains('m')); // give the true false value corresponds contain element 'm'
+  // print(abc.contains('e'));
+  // print(abc.padLeft(10));
+  // print(abc.padRight(10));
+
+  // var str1 = '    World   ';
+  // print(str1);
+  // print(str1.trim()); // remove the unnecessary space
+  // var str2 = 'Hello%World';
+  // print(str2.split('%')); // spilt the given symbol
+
+  // 5 : List
+  //var lst = new List(4);
+
+  var lst = [];
+  lst.add("hii");
+  lst.add('hello');
+  print(lst);
+  print(lst.length);
+  print(lst[1]);
 }
